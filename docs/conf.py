3b79460bbfd996480e0dd4a882a32d042c48e93d
Franz Höpfinger <mail@franzhoepfinger.de>
2023-01-29 14:50:11 +0100

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,13 @@ extensions = ["myst_parser"]
 version = "0.0.1"
 release = version
 templates_path = ["_templates"]
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
@@ -46,5 +53,12 @@ myst_heading_anchors = 7
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
